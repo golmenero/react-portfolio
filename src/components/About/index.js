576c01dafdc526bba85b7278ebb5e0a4518784e5
Carlos Gómez <carlosgomezcolmenero@gmail.com>
2022-11-16 21:23:34 +0100

    Change About section with new version

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,24 +4,25 @@ const About = () => {
     
     return (
         <section id="about">
-            <div className='about-page'>
-                <div className='section-card header-zone'>
-                    <h1 className='text-large justify'>
-                        I'm Carlos, a Software Engineer. I love programming to make the world a simpler place.
+            <div className='about-page p-5em flex'>
+                <div class="w-75 mr-5em">
+                    <h1 className='text-xl justify'>
+                        Hello! <br/> I'm Carlos, a Software Engineer.
                     </h1>
-                    <div className='header-zone-subtext'>
-                        <div className='text-medium'>
-                            <p className='text-justify'>
-                            I am a young Engineer with a passion for programming. Since I was a kid I have been obsessed with solving problems, and code development allows me to fulfill my dream of unleashing my creativity to put a solution to any problem in the world, while making people's lives easier. any problem in the world, while making people's lives easier.
-                            </p>
-                            <span class="quote">"Build the simplest code, solve the biggest problems"</span>
-                        </div>
-                        <img className='w-25' alt="Profile picture" src='logo.png'/>
+                    <div className='about-subtext'>
+                        <p className='text-justify text-m'>
+                            I am a young Engineer with a passion for programming. I graduated in 2021 from the University of Oviedo, at the School of Computer Engineering. Since I was a kid I have been obsessed with solving problems, and code development allows me to fulfill my dream of unleashing my creativity to put a solution to any problem in the world, while making people's lives easier. any problem in the world, while making people's lives easier.
+                        </p>
+                        <p class="quote text-l">
+                            "Build the simplest code, solve the most complex problems"
+                        </p>
                     </div>
+                </div>
+                <div class="w-25">
+                    <img alt="Profile picture" src='logo.jpeg'/>
                 </div>
             </div>
         </section>
-
     )
 }
 
