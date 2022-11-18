962a6a3f68fae41f2b7121b07d81a2fc06c9b258
Carlos Gómez <carlosgomezcolmenero@gmail.com>
2022-11-18 22:01:04 +0100

    Change overall style for project (prototype version

--- src/components/Projects/Project/index.js
+++ src/components/Projects/Project/index.js
@@ -3,11 +3,10 @@ import './index.scss';
 const Project = ({ link, title, subtitle, img}) => {
     
     return (
-        <div className='project'>
+        <div className={`project ${img}`}>
             <div>
                 <h3 className='text-m bold-600'>{title}</h3>
                 <p className='text-s'>{subtitle}</p>
-                <img src={img} alt={title}></img>
             </div>
         </div>                 
     )
