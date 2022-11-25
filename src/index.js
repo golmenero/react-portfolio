5f952730f444f170c85eb7bc3afeb4332e9fdb04
Carlos Gómez <carlosgomezcolmenero@gmail.com>
2022-11-26 00:34:31 +0100

    Relative paths

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import './App.scss';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
     <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
