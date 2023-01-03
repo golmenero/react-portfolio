0dc92641c0d6bad25cec6787972d537ff6563563
Carlos Gómez <carlosgomezcolmenero@gmail.com>
2023-01-03 01:36:26 +0100

    Change project images

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,21 +1,24 @@
 import { Button, Col, Row } from 'reactstrap';
 import './index.scss';
 import { useTranslation } from "react-i18next";
+import Tilt from 'react-parallax-tilt';
 
 const Project = ({ pos, link, title, subtitle, img}) => {
     const [t] =  useTranslation("global");
 
     return (
-        <Row className="project animated-border section-color p-2em h-100">
-            <Col sm="12" md="6">
-            <h2 className='heading-2'>{ title }</h2>
-                <p className='heading-3'> { subtitle }</p>
-                <Button className='custom-button' href={link}>{ t("projects.see-source-code") }</Button>
-            </Col>
-            <Col sm="12" md="6" className='text-center'>
-                    <img className='animated-border' src={ img } alt={ title } />
-            </Col>
-        </Row>
+        <Tilt className='h-100' tiltMaxAngleX="1" tiltMaxAngleY="1" scale="1.01">
+            <Row className="project section-color p-2em h-100">
+                <Col sm="12" md="6">
+                <h2 className='heading-2'>{ title }</h2>
+                    <p className='heading-3'> { subtitle }</p>
+                    <Button className='custom-button' href={link}>{ t("projects.see-source-code") }</Button>
+                </Col>
+                <Col sm="12" md="6" className='text-center'>
+                        <img src={ img } alt={ title } />
+                </Col>
+            </Row>
+        </Tilt>
     )
 }
 
@@ -28,10 +31,10 @@ const Projects = () => {
                 <h1 className='heading-1'>{ t("navigation.projects") }</h1>
             </Row>
             <div>
-                <Project pos='L' link="https://github.com/golmenero/tfg_inmobiliaria" img="projects/proj-3-raw.png" title={ t("projects.realestate.title") } subtitle={ t("projects.realestate.subtitle") }></Project>
-                <Project pos='R' link="https://github.com/golmenero/javascript_videogame" img="projects/proj-4-raw.png" title={ t("projects.runner.title") } subtitle={ t("projects.runner.subtitle") }></Project>                   
-                <Project pos='L' link="https://github.com/golmenero/videojuego_shooter" img="projects/proj-2-raw.jpg" title={ t("projects.shooter.title") } subtitle={ t("projects.shooter.subtitle") }></Project>
-                <Project pos='R' link="https://github.com/golmenero/sdi505-lab-spring" img="projects/proj-1-raw.jpg" title={ t("projects.music.title") } subtitle={ t("projects.music.subtitle") }></Project>                       
+                <Project pos='L' link="https://github.com/golmenero/tfg_inmobiliaria" img="projects/proj-3-new.png" title={ t("projects.realestate.title") } subtitle={ t("projects.realestate.subtitle") }></Project>
+                <Project pos='R' link="https://github.com/golmenero/javascript_videogame" img="projects/proj-4-new.png" title={ t("projects.runner.title") } subtitle={ t("projects.runner.subtitle") }></Project>                   
+                <Project pos='L' link="https://github.com/golmenero/videojuego_shooter" img="projects/proj-2-new.png" title={ t("projects.shooter.title") } subtitle={ t("projects.shooter.subtitle") }></Project>
+                <Project pos='R' link="https://github.com/golmenero/sdi505-lab-spring" img="projects/proj-1-new.png" title={ t("projects.music.title") } subtitle={ t("projects.music.subtitle") }></Project>                       
             </div>
         </section>
     )
