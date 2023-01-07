a9ae4ebc7184684356f3c03d04f8960ab44f2c38
Carlos Gómez <carlosgomezcolmenero@gmail.com>
2023-01-07 04:53:02 +0100

    Add 3D rendering model

---
+++ src/components/3D/GltfModel.js
@@ -0,0 +1,23 @@
+import React, { useRef } from "react";
+import { useLoader, useFrame } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+
+const GltfModel = ({ modelPath, scale = 40, position = [0, 0, 0] }) => {
+	const ref = useRef();
+	const gltf = useLoader(GLTFLoader, modelPath);
+
+	// Subscribe this component to the render-loop, rotate the mesh every frame
+	useFrame((state, delta) => (ref.current.rotation.y += 0.001));
+	return (
+		<>
+			<primitive
+				ref={ref}
+				object={gltf.scene}
+				position={position}
+				scale={scale}
+			/>
+		</>
+	);
+};
+
+export default GltfModel;
---
+++ src/components/3D/ModelViewer.js
@@ -0,0 +1,22 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import GltfModel from "./GltfModel";
+import { OrbitControls } from "@react-three/drei";
+
+const ModelViewer = ({ modelPath, scale = 45, position = [0, 0, -3] }) => {
+	return (
+		<Canvas className="canvas">
+			<ambientLight />
+			<spotLight position={[10, 10, 10]} angle={1} intensity={0.75} />
+			<spotLight position={[10, 10, -10]} angle={1} intensity={1} />
+			<spotLight position={[-10, -10, 10]} angle={1} intensity={1} />
+			<spotLight position={[-10, -10, -10]} angle={1} intensity={0.75} />
+			<Suspense fallback={null}>
+				<GltfModel modelPath={modelPath} scale={scale} position={position} />
+				<OrbitControls enableZoom={false} target={[0, 0, -3]} />
+			</Suspense>
+		</Canvas>
+	);
+};
+
+export default ModelViewer;
