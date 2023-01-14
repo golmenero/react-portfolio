6506a965606f7e02d2775f60659574bedd10bc23
Carlos Gómez <carlosgomezcolmenero@gmail.com>
2023-01-14 01:51:20 +0100

    Remove line SVG. Add parallax animation to first section

--- src/App.js
+++ src/App.js
@@ -51,19 +51,11 @@ function App() {
 		navigation_elements.forEach((item) => {
 			observer_nav.observe(item);
 		})
-	});
 
-	useEffect(() => {
-		let path = document.querySelector('path');
-		let pathLength = path.getTotalLength();
+		window.addEventListener('scroll', function() {
+			let value = window.scrollY;
 
-		path.style.strokeDasharray = pathLength + " " + pathLength;
-		path.style.strokeDashoffset = pathLength;
-
-		window.addEventListener('scroll', () => {
-			let scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
-			var drawLength = pathLength * scrollPercentage;
-			path.style.strokeDashoffset = pathLength - drawLength;
+			this.document.getElementById('content-home').style.top = value * 0.7 + "px";
 		})
 	});
 
@@ -74,15 +66,10 @@ function App() {
 			</header>
 			<main>
 				<div className="ball"></div>
-				<div className='gradient-color'>
+				<div className='primary-color'>
 					<Container>
 						<Home />
 					</Container>
-				</div>
-				<div className='line-container'>
-					<svg viewBox="0 0 255 1858" fill="none" preserveAspectRatio='xMidYMax meet'>
-						<path d="M101.5 -0.5V336C101.5 348.333 101.5 373.9 101.5 377.5C101.5 382 18.5 476.5 101.5 504C184.5 531.5 159.159 390 83 390C59.5 390 48.5 445.333 49 476.5C52.3333 506.667 66.7 516.9 101.5 540.5C136.3 564.1 136.5 646.833 122 675C115.333 680.333 100.3 744.1 101.5 750.5C102.7 756.9 95.8333 790.333 101.5 812L138.673 932.5C142.173 978 160.145 1016 156 1078.5C149.5 1176.5 29.5 1180 8 1078.5C-9.2 997.3 25 985 37.5 1028.5C38 1050.83 30.8087 1080.63 19.5 1119.5C11.5 1147 145.5 1182 140 1224.5C134.5 1267 118.5 1295 101.5 1327C84.5 1359 101.5 1361 101.5 1411.5C101.5 1451.9 101.5 1531.67 101.5 1566.5V2095" stroke="#171719" strokeWidth="5"/>
-					</svg>
 				</div>
 				<div className='secondary-color'>
 					<Container className='mt-0'>
@@ -96,7 +83,7 @@ function App() {
 				<Socials />
 			</main>
 			<footer>
-				<Footer className='gradient-color inverted'/>
+				<Footer />
 			</footer>
 		</>
 	);
--- src/components/Home.js
+++ src/components/Home.js
@@ -7,25 +7,27 @@ const Home = () => {
 
 	return (
 		<section id="home" data-nav="home" className='hidden'>
-			<Row>
-				<Col sm="12" md="12" lg="7">
-					<h1 className='heading-2 mt-0'>
-						<span className='accent-color'>{t("home.hello")}</span>
-					</h1>
-					<h1 className='heading-1 mt-0'>
-						{t("home.header")}
-					</h1>
-					<div>
-						<p className='heading-3'>{t("home.description")}</p>
-						<p className="quote text-l">{t("home.quote")}</p>
-					</div>
-				</Col>
-				<Col sm="12" md="12" lg="5">
-					<ModelViewer modelPath={"logo/logo.glb"} />
-				</Col>
-			</Row>
-			<div id="more" className='text-center'>
-				<a href="#about"> {t("home.more")} <i className='bx bx-down-arrow-alt bx-fade-down' ></i></a>
+			<div id='content-home'>
+				<Row>
+					<Col sm="12" md="12" lg="7">
+						<h1 className='heading-2 mt-0'>
+							<span className='accent-color'>{t("home.hello")}</span>
+						</h1>
+						<h1 className='heading-1 mt-0'>
+							{t("home.header")}
+						</h1>
+						<div>
+							<p className='heading-3'>{t("home.description")}</p>
+							<p className="quote text-l">{t("home.quote")}</p>
+						</div>
+					</Col>
+					<Col sm="12" md="12" lg="5">
+						<ModelViewer modelPath={"logo/logo.glb"} />
+					</Col>
+				</Row>
+				<div id="more" className='text-center'>
+					<a href="#about"> {t("home.more")} <i className='bx bx-down-arrow-alt bx-fade-down' ></i></a>
+				</div>
 			</div>
 		</section>
 
