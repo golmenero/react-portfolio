89ed9ddefce1bacfe86ba9f92552f59b74c46627
Carlos Gómez <carlosgomezcolmenero@gmail.com>
2023-01-15 23:03:45 +0100

    Change size of model and threshold of rotation

--- src/components/3D/GltfModel.js
+++ src/components/3D/GltfModel.js
@@ -9,8 +9,8 @@ const GltfModel = ({ modelPath, scale = 10, position = [0, 0, 0] }) => {
 	useFrame((state, delta) => {
 		let value = Math.round((ref.current.rotation.y + Number.EPSILON) * 100) / 100;
 		if (value % 6.28 === 0) ref.current.rotation.y = 0;
-		if (value >= 3.7 && value <= 5.5) ref.current.rotation.y += 0.001;
-		else ref.current.rotation.y += 0.009;
+		if (value >= 4.2 && value <= 5.2) ref.current.rotation.y += 0.001;
+		else ref.current.rotation.y += 0.01;
 	});
 
 	return (
