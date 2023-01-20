cf3fa5632d4f164c0d233aa9a547bf6eb74ef9c6
Carlos Gómez <carlosgomezcolmenero@gmail.com>
2023-01-20 15:35:24 +0100

    Change lights of 3D model

--- src/components/3D/ModelViewer.js
+++ src/components/3D/ModelViewer.js
@@ -6,12 +6,13 @@ const ModelViewer = ({ modelPath, scale = 35, position = [0, 1, 0] }) => {
 	return (
 		<Canvas className="canvas">
 			<ambientLight />
-			<spotLight position={[10, 10, -10]} angle={1} intensity={1} />
-			<spotLight position={[-10, -10, 10]} angle={1} intensity={1} />
+			<spotLight position={[10, 10, 10]} angle={0.7} intensity={1} />
+			<spotLight position={[0, 0, 10]} angle={0.4} intensity={1} />
 			<Suspense fallback={null}>
 				<GltfModel modelPath={modelPath} scale={scale} position={position} />
 			</Suspense>
 		</Canvas>
+
 	);
 };
 
